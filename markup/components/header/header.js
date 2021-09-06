8038199b7d6df8dfb20467f1a95d011afe0d7c6f
Pavel Ananiev <pavel.ananiev@rsquad.io>
2021-09-06 16:50:15 +0300

    update markup

--- markup/components/header/header.js
+++ markup/components/header/header.js
@@ -1,1 +1,18 @@
-
+const header = document.getElementById('header');
+console.log(header);
+const scrolledCheck = () => {
+    const scrollY = window.scrollY;
+    if (scrollY && !header.className.includes('_scrolled')) {
+        header.className = header.className + ' _scrolled';
+    }
+    if (!scrollY && header.className.includes('_scrolled')) {
+        header.className = header.className.replace(' _scrolled', '');
+    }
+};
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('kekw');
+    scrolledCheck();
+});
+document.onscroll = () => {
+    scrolledCheck();
+};
