048d84d354fb2d9f259970c38dfbe02cc8542dc5
Pavel Ananiev <pavel.ananiev@rsquad.io>
2021-09-08 01:44:09 +0300

    add links in partners

--- markup/components/partners/partners.js
+++ markup/components/partners/partners.js
@@ -1,1 +1,12 @@
-
+const digiuLogo = document.getElementById('digiu');
+const brLogo = document.getElementById('br');
+const rsquadLogo = document.getElementById('rsquad');
+digiuLogo.onclick = () => {
+    window.open('http://digiu.ai');
+};
+brLogo.onclick = () => {
+    window.open('https://br.capital/');
+};
+rsquadLogo.onclick = () => {
+    window.open('https://rsquad.io/');
+};
