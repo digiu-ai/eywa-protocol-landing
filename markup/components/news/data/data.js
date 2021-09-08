d72eb25b609fb998b2f48c24686be5915b349697
Pavel Ananiev <pavel.ananiev@rsquad.io>
2021-09-08 18:02:41 +0300

    fix dates

--- markup/components/news/data/data.js
+++ markup/components/news/data/data.js
@@ -2,27 +2,27 @@ var data = {news: {
   list: [
     {
       img: '%=static=%img/assets/news/preparing.png',
-      date: '27 august',
+      date: '7 september',
       text: 'EYWA Cross-chain prepares the DeFi Revolution.',
       href: 'https://medium.com/@eywaprotocol/eywa-cross-chain-prepares-the-defi-revolution-86c1c3b2303c',
     },
     {
-      img: '%=static=%img/assets/news/cross-chain.png',
-      date: '27 august',
-      text: 'Cross-chain protocol and crypto exchange.',
-      href: 'https://medium.com/@eywaprotocol/cross-chain-protocol-and-crypto-exchange-9c0e23427115',
-    },
-    {
       img: '%=static=%img/assets/news/so-high.png',
-      date: '27 august',
+      date: '4 september',
       text: 'Why are fees in DeFi so high?',
       href: 'https://medium.com/@eywaprotocol/why-are-fees-in-defi-so-high-b19556327823',
     },
     {
       img: '%=static=%img/assets/news/why-need.png',
-      date: '27 august',
+      date: '4 september',
       text: 'Why does the DeFi market need a cross-chain protocol?',
       href: 'https://medium.com/@eywaprotocol/why-are-fees-in-defi-so-high-ed03dee3770a',
+    },
+    {
+      img: '%=static=%img/assets/news/cross-chain.png',
+      date: '3 september',
+      text: 'Cross-chain protocol and crypto exchange.',
+      href: 'https://medium.com/@eywaprotocol/cross-chain-protocol-and-crypto-exchange-9c0e23427115',
     },
   ]
 }};
