5aa06fcf52ffcb038424199855df9ba89ec62974
Pavel Ananiev <pavel.ananiev@rsquad.io>
2021-10-22 16:54:35 +0300

    fix url for production

--- markup/components/subscribe/subscribe.js
+++ markup/components/subscribe/subscribe.js
@@ -8,7 +8,7 @@ for (let i = 0; i < subscribeForms.length; i++) {
         const data = {
             email: e.srcElement[0].value
         };
-        xhr.open('POST', 'https://api.dev.eywa.fi/subscription-email-lists', true);
+        xhr.open('POST', 'https://api.eywa.fi/subscription-email-lists', true);
         xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
         xhr.onload = () => {
             e.srcElement[0].value = '';
